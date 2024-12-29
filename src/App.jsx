a9b3f2d8ed3fcdd353f05e2c46925c81936b3424
Dhawal Dyavanpalli <dhawalhost@gmail.com>
2024-12-29 23:11:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import './App.css'
+// import Dashboard from './dashboard/Dashboard'
+// import SignUp from './signup/SignUp'
+import LandingPage from './landingPage/LandingPage'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+     {/* <Dashboard /> */}
+     <LandingPage />
+     {/* <SignUp /> */}
+    </>
+  )
+}
+
+export default App
